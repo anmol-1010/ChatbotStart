8028a40a528d591548634e4f01a023970872db8f
Anmol <avirdi7298@conestogac.on.ca>
2022-09-30 23:16:35 -0400

    tempCommit

--- assignment1Shwarma.js
+++ assignment1Shwarma.js
@@ -30,25 +30,25 @@ module.exports = class ShwarmaOrder extends Order{
             case OrderState.WELCOMING:
                 this.stateCur = OrderState.ITEM;
                 aReturn.push("Welcome to Wonder Foods.");
-                aReturn.push("What would you like to order today? /n Enter 1 for Burger or Enter 2 for Rolls");
+                aReturn.push("What would you like to order today? \n Enter `1` for Burger \n Enter `2` for Rolls");
                 break;
             case OrderState.ITEM:
                     if (sInput== 1)
                     {
                         this.stateCur = OrderState.SIZE_1;
                         this.sItem1="Burger";
-                        aReturn.push("What size burger would you like? --> *Small* OR *Medium* OR *Large*");
+                        aReturn.push("What size burger would you like? \n --> Small \n --> Medium \n --> OR Large");
                     }
                     else if (sInput==2) 
                     {
                         this.stateCur = OrderState.SIZE_2;
                         this.sItem2="Rolls";
-                        aReturn.push("What size Roll would you like? --> *Small* OR *Medium* OR *Large*");
+                        aReturn.push("What size Roll would you like? \n --> Small \n --> Medium \n --> Large");
                     } 
                     else if (this.sItem1 == "Burger" || this.sItem2 == "Rolls")
                     {
                         this.stateCur = OrderState.ICECREAM;
-                        aReturn.push("Would you like any icecream with that? /n YES or NO?")
+                        aReturn.push("Would you like any icecream with that? \n YES or NO ?")
                     }
                     else{
                         this.isDone(true);
@@ -60,21 +60,21 @@ module.exports = class ShwarmaOrder extends Order{
                     {
                     this.stateCur = OrderState.BREAD_TYPE;
                     this.sSize1 = "Small";
-                    this.sPrice+=5;//burger cost
-                    aReturn.push("What type of bread would you like for Burger? --> *Wheat* OR *Multigrain*");
+                    this.sPrice += 5;
+                    aReturn.push("What type of bread would you like for Burger? \n --> Wheat \n --> Multigrain");
                     }
                     else if(sInput=="medium"||sInput=="Medium"||sInput=="MEDIUM")
                     {
                         this.stateCur = OrderState.BREAD_TYPE;
                         this.sSize1 = "Medium";
-                        this.sPrice+=5.5;
-                        aReturn.push("What type of bread would you like for Burger? --> *Wheat* OR *Multigrain*");
+                        this.sPrice += 5.5;
+                        aReturn.push("What type of bread would you like for Burger? \n --> Wheat \n --> Multigrain");
                     }
                     else if(sInput == "large" || sInput == "Large" || sInput=="LARGE"){
                         this.stateCur = OrderState.BREAD_TYPE;
                         this.sSize1 = "Large";
-                        this.sPrice+=6;
-                        aReturn.push("What type of bread would you like for Burger? --> *Wheat* OR *Multigrain*");
+                        this.sPrice += 6;
+                        aReturn.push("What type of bread would you like for Burger? \n --> Wheat \n --> Multigrain");
                     }
                 break;
                     case OrderState.SIZE_2:
@@ -82,67 +82,66 @@ module.exports = class ShwarmaOrder extends Order{
                     {
                     this.stateCur = OrderState.TOPPINGS;
                     this.sSize2 = "Small";
-                    this.sPrice += 4.5;// roll cost
-                    aReturn.push("What type of Topping would you like for the roll? --> *Onion* OR *Cheese*");
+                    this.sPrice += 4.5;
+                    aReturn.push("What type of Topping would you like for the roll? \n --> Onion \n --> Cheese");
                     }
                     else if(sInput == "medium" || sInput == "Medium" || sInput=="MEDIUM")
                     {
                     this.stateCur = OrderState.TOPPINGS;
                     this.sSize2 = "Medium";
-                    this.sPrice += 5;// roll cost
-                    aReturn.push("What type of Topping would you like for the roll? --> *Onion* OR *Cheese*");
+                    this.sPrice += 5;
+                    aReturn.push("What type of Topping would you like for the roll? \n --> Onion \n --> Cheese");
                     }
                     else if(sInput == "large" || sInput == "Large" || sInput=="LARGE")
                     {
                     this.stateCur = OrderState.TOPPINGS;
                     this.sSize2 = "Large";
-                    this.sPrice += 5.5;// roll cost
-                    aReturn.push("What type of Topping would you like for the roll? --> *Onion* OR *Cheese*");
+                    this.sPrice += 5.5;
+                    aReturn.push("What type of Topping would you like for the roll? \n --> Onion \n --> Cheese");
                     }
                 break;
                     case OrderState.BREAD_TYPE:
-                    // this.sBreadType = sInput;
+                    
                 if(sInput=="wheat"||sInput=="Wheat"||sInput=="WHEAT" )
                 {
                     this.stateCur = OrderState.ITEM;
                     this.sBreadType="Wheat";
-                    this.sPrice+=1.5;//bread cost
-                    aReturn.push("Type 2 if you Would like to order roll? OR No to end your order");
+                    this.sPrice+=1.5;
+                    aReturn.push("Type 2 if you Would like to order roll? \n OR No to end your order");
                 }
                 else if(sInput=="multigrain"||sInput=="Multigrain"||sInput=="MULTIGRAIN" )
                 {
                     this.stateCur = OrderState.ITEM;
                     this.sBreadType="Multigrain";
-                    this.sPrice+=2;//add price for italian bread
-                    aReturn.push("Type 2 if you Would like to order roll? OR No to end your order");
+                    this.sPrice+=2;
+                    aReturn.push("Type 2 if you Would like to order roll? \n OR No to end your order");
                 }
                 else
                 {
                     this.stateCur = OrderState.ICECREAM;
-                    aReturn.push("Would you like an icecream Softie with that? Type Yes OR No to end your order");
+                    aReturn.push("Would you like an icecream Softie with that? \n Type Yes OR No to end your order");
                 }
             break;
             case OrderState.TOPPINGS:
-                //this.sToppings = sInput;
 
               if(sInput== "cheese"||sInput== "Cheese"||sInput== "CHEESE")
                 {
                     this.stateCur = OrderState.ITEM;
                     this.sToppings = "Cheese";
-                    this.sPrice+=1;//topping cost
-                    aReturn.push("Type 1 if you Would like to order Burger OR No to end your order");
+                    this.sPrice+=1;
+                    aReturn.push("Type 1 if you would like to order Burger \n OR No to end your order");
                 }
                 else if(sInput== "onion"||sInput== "Onion"||sInput== "ONION")
                 {
                     this.stateCur = OrderState.ITEM;
                     this.sToppings = "Onion";
                     this.sPrice+=1.5;
-                    aReturn.push("Type 1 if you Would like to order Burger as well? OR No to end your order");
+                    aReturn.push("Type 1 if you would like to order Burger as well? \n OR No to end your order");
                 }
                 else
                 {
                     this.stateCur = OrderState.ICECREAM;
-                    aReturn.push("Would you like an Icecream Softie with that? Type Yes OR No to end your order");
+                    aReturn.push("Would you like an Icecream Softie with that? \n Type Yes OR No to end your order");
                 }
                 break;
                 case OrderState.ICECREAM:
@@ -150,9 +149,9 @@ module.exports = class ShwarmaOrder extends Order{
                     if(sInput.toLowerCase() != "no")
                     {
                         this.sIcecream = sInput;
-                        this.sPrice += 2;//softie cost
+                        this.sPrice += 2;
                     }
-                    aReturn.push("Thank-you for your order of :--");
+                    aReturn.push("Your order payment receipt :");
                     if (this.sItem1 != "")
                     {
                         aReturn.push(`${this.sSize1} ${this.sItem1} with ${this.sBreadType} bread`);
@@ -163,10 +162,10 @@ module.exports = class ShwarmaOrder extends Order{
                     }
                     if(this.sIcecream)
                     {
-                        aReturn.push("With Icecream Softie");
+                        aReturn.push("With Vanilla Icecream");
                     }
                     let d = new Date(); 
-                    aReturn.push(`Total Bill to be paid is $${this.sPrice}`)
+                    aReturn.push(`Total amount to be paid is $${this.sPrice}`)
                     d.setMinutes(d.getMinutes() + 20);
                     aReturn.push(`Your food will be ready by ${d.toTimeString()}`);
                     break;
